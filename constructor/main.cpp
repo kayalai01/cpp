86d1a05cc7bc01d32b5f13df64d7559908e7f8f9
Mkayalai01 <mkayalai01@gmail.com>
2024-01-20 13:20:56 +0530

    add constructor file

---
+++ constructor/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+ class constructor
+ {
+ public :
+    constructor ()
+    {
+    string Name ;
+    cout << "Enter a Candidate Name :" << endl;
+    cin >> Name ;
+    cout << "Candidate Name :"<< Name << endl;
+
+
+
+
+    }
+
+ };
+
+int main ()
+ {
+     constructor cons;
+     return 0;
+ }
